f8a2a0fe9bc2d3803fe7f6e33d169e76c6c10753
Alexandr <nook17n1@gmail.com>
2026-06-07 13:18:02 +0300

    Solution1

--- task.sh
+++ task.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-mysqldump -u "$DB_USER" -p"$DB_PASSWORD" ShopDB --no-create-db --result-file=backup-no-create-db.sql
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" ShopDB --no-create-db > backup-no-create-db.sql
 mysql -u "$DB_USER" -p"$DB_PASSWORD"  ShopDBReserve < backup-no-create-db.sql
-mysqldump -u "$DB_USER" -p"$DB_PASSWORD" ShopDB --no-create-info --result-file=backup-no-create-info.sql
-mysql -u "$DB_USER" -p"$DB_PASSWORD"  ShopDBDevelopment < backup-no-create-info.sql
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" ShopDB --no-create-info > backup-no-create-info.sql
+mysql -u "$DB_USER" -p"$DB_PASSWORD" --databases ShopDBDevelopment < backup-no-create-info.sql
